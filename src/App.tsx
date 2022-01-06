8d9c221203fe4057b5ebe0a890e82a16e777969d
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-01-06 21:31:39 +0000

    Integer props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Greet } from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet name="Harry"/>
+      <Greet name="Harry" notification={15}/>
     </div>
   )
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,12 @@
 type GreetProps = {
     name: string;
+    notification: number;
 }
 
 export const Greet = (props: GreetProps) => {
     return (
         <div>
-            <h2>Welcome {props.name}! You have 10 undread messages.</h2>
+            <h2>Welcome {props.name}! You have {props.notification} unread messages.</h2>
         </div>
     )
 }
