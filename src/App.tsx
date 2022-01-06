9ce372320a3c3be5f88b2ea79b4202b3dab50e3c
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-01-06 21:48:54 +0000

    Array of names

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
   const personName = {
     first: 'Harry',
     last: 'Hopkinson'
   }
+
+  const nameList = [
+    {
+      first: "Person",
+      last: "One"
+    },
+    {
+      first: "Person",
+      last: "Two"
+    },
+    {
+      first: "Person",
+      last: "Three"
+    }
+  ]
+
   return (
     <div className="App">
       <Greet name="Harry" notification={15} isLoggedIn={true}/>
       <Person name={personName}/>
+      <PersonList names={nameList}/>
     </div>
   )
 }
