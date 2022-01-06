3290b13cec42f232fad06b705912ae13be906302
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-01-06 21:37:07 +0000

    Used if statements

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,12 +1,17 @@
 type GreetProps = {
     name: string;
     notification: number;
+    isLoggedIn: boolean;
 }
 
 export const Greet = (props: GreetProps) => {
     return (
         <div>
-            <h2>Welcome {props.name}! You have {props.notification} unread messages.</h2>
+            <h2>
+                {
+                    props.isLoggedIn ?  "Welcome ${props.name}! You have ${props.notification} new notifications" : "Welcome Guest"
+                }
+            </h2>
         </div>
     )
 }
